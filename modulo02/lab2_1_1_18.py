70e5f963903b515409333f02a347e88846f772ad
Marianacrmz <marianacramirez.18@gmail.com>
2022-09-20 09:44:52 -0500

    Lab 2.1.1.18 y 2.1.1.19

---
+++ modulo02/lab2_1_1_18.py
@@ -0,0 +1,7 @@
+'''
+Descripción: Programa muy fácil de la función print()
+Autor: Mariana Canchola Ramírez
+Fecha: 20 sep 2022
+'''
+print("Fundamentos","Programación","en",sep="***",end="...")
+print("Python")
---
+++ modulo02/lab2_1_1_19.py
@@ -0,0 +1,30 @@
+'''
+Descripción: Hacer la flecha al doble y duplicarla de lado a lado.
+Autor: Mariana Canchola Ramírez
+Fecha: 20 sep 2022
+'''
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+
+print("Flecha al doble")
+print("           *")
+print("         *  *")
+print("        *    *")
+print("       *      *")
+print("      *        *")
+print("     *          *")
+print("    ***        ***")
+print("      *        *")
+print("      *        *")
+print("      *        *")
+print("      *        *")
+print("      **********")
+
