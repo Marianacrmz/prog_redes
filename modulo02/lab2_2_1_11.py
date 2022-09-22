d49ff37ab8a62d6bcc7efbff85e80e8c20d7143a
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-09-22 17:52:20 -0500

    Laboratorios

---
+++ modulo02/lab2_2_1_11.py
@@ -0,0 +1,11 @@
+'''
+Descripción: En una sola linea de codigo, obtener:
+"Estoy"
+""aprendiendo""
+"""Python""" 
+
+Autor: Mariana Canchola Ramírez
+Fecha: 22 sep 2022
+'''
+
+print("\"Estoy\"",'""aprendiendo""','"""Python"""',sep="\n")
---
+++ modulo02/lab2_6_1_10.py
@@ -0,0 +1,11 @@
+'''
+Descripción: Operadores y expresiones
+Autor: Mariana Canchola Ramírez
+Fecha: 22 sep 2022
+'''
+
+x = float(input("Ingresa el valor para x: "))
+
+y = 1/(x+(1/(x+(1/(x+(1/x))))))
+
+print("y =", y)
