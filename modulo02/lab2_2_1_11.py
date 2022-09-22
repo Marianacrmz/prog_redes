0188033fd50bf940956994536f3f4d32bb6aa959
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-09-22 09:37:43 -0500

    Laboratorio

---
+++ modulo02/lab2_2_1_11.py
@@ -0,0 +1,10 @@
+'''
+Descripción: En una slola linea de codigo, obtener 
+"Estoy"
+""aprendiendo""
+"""Python""" 
+Autor: Mariana Canchola Ramírez
+Fecha: 22 sep 2022
+'''
+
+print("\"Estoy\" \n\"\"aprendiendo\"\" \n\"\"\"Python\"\"\"")
