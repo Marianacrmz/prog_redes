b0e6f522d4c8047eea4706eadbcfce5ca68ef301
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-09-29 10:17:44 -0500

    laboratorios

--- modulo03/lab3.1.1.11.py
+++ modulo03/lab3.1.1.11.py
@@ -1,15 +1,17 @@
-
-
-
-
+'''
+Descripción: Fundamentos de la sentencia if-else
+Autor: Mariana Canchola Ramírez
+Fecha: 27 sep 2022
+'''
 
 income = float(input("Introduce el ingreso anual:"))
 tax = 0
 if income < 85528:
     tax = income *0.18 -556.02
 else: 
-    tax = 14839.02 * 0.32 * (income - 85528)
+    tax = 14839.02 + (0.32*(income - 85528))
 tax = round(tax, 0)
 if tax < 0:
     tax = 0.0
 print("El impuesto es:", tax, "pesos")
+
