bbcfc33afe11b09cd549b5822b2e74eb2e2a45c4
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-10-11 09:52:01 -0500

    Ejercicios CORREGIDOS y AÑADIDOS

--- unidad1_practico/u1_prob1.py
+++ unidad1_practico/u1_prob1.py
@@ -1,20 +1,23 @@
 '''
-Descripción: Raíces de la ecuación cuadrática
+Descripción: Jugando con listas
+Escribir un programa que almacene las asignaturas de un curso en una lista, pregunte al usuario la nota
+que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje: 
+En <asignatura> has sacado <nota> donde <asignatura> es cada una de las asignaturas de la
+lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
 Autor: Mariana Canchola Ramírez
-Fecha: 06 octubre 2022
+Fecha: 11 octubre 2022
 '''
 
-from math import sqrt
-a = int(input("Ingrese a"))
-b = int(input("Ingrese b"))
-c = int(input("Ingrese c"))
-x1= 0
-x2= 0
-if ((b**2)-4*a*c) < 0:
-  print("La solución es: ")
-else:
-  x1 = (-b+sqrt(b**2-(4*a*c)))/(2*a)
-  x2 = (-b-sqrt(b**2-(4*a*c)))/(2*a)
-  print(x1)
-  print(x2)
-  
+print ("Bienvenido, ingresa las calificaciones que obtubiste")
+
+#Creamos una lista con las materias
+mi_lista = ["Conexion de redes", "Programación de Redes", "Electronica para IdC", "Inglés"] 
+#Creamos la variable y lista vacia de las calificaciones que ingresará el usuario
+calificaciones = []
+
+#Con una función insertamos las materias en la lista para que nos arroje un texto con cada una de las materias.
+for materias in mi_lista:
+    calificacion = input("¿Qué calificación sacaste en " + materias + "?\n")
+    calificaciones.append(calificacion) #Agregamos un append para que el usuario ingrese la calificación y se almacenen en la lista llamada "calificacion"
+for i in range(len(mi_lista)): #Coloqué un range, con la variable i, para que me cree una secuencia de la lista de las materias
+    print("\nEn " + mi_lista[i] + " has sacado " + calificaciones[i]) #Finalmente imprimo la materia, con el nombre de cada una de las materias, y su calificación.
--- unidad1_practico/u1_prob4.py
+++ unidad1_practico/u1_prob4.py
@@ -1,13 +1,17 @@
+'''
+Descripción: Partiendo fecha
+Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato
+dd/mm/aaaa y muestra por pantalla, el día, el mes y el año.
+Autor: Mariana Canchola Ramírez
+Fecha: 11 octubre 2022
+'''
 
-mi_lista = []
-print("Nombres:")
-    
-mi_lista.append ("Luis")
-mi_lista.append ("Pedro")
-mi_lista.append ("Paco")
-mi_lista.append ("Arturo")
-print(mi_lista)
+print ("Bienvenido\n")
 
-for i in range(1):
-    mi_lista.append (input("Agrega un nombre: "))
-print(mi_lista)
+mes = {1:"Enero", 2:"Febrero", 3:"Marzo", 4:"Abril", 5:"Mayo", 6:"Junio", 7:"Julio", 8:"Agosto", 9:"Septiembre", 10:"Octubre", 11:"Noviembre", 12:"Diciembre"}
+print ("Escribe tu fecha de nacimiento en formato dd-mm-aaaa")
+str = input("Dime cuando naciste: ")
+
+str = str.split('-')
+
+print("Tu naciste el: ",str[0], "de", mes[int(str[1])], "de", str[2])
