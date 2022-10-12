8953d59007904ab8166f9bb85b300b669430c95e
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-10-12 17:31:53 -0500

    Update lab4.3.1.7.py

--- modulo04/lab4.3.1.7.py
+++ modulo04/lab4.3.1.7.py
@@ -1,9 +1,12 @@
 '''
 Descripción: ¿Cuántos días?: escribiendo y utilizando tus propias funciones
+escribir y probar una función que toma dos argumentos (un año y un mes) y devuelve 
+el número de días del mes/año dado (mientras que solo febrero es sensible al valor year, tu función debería ser universal)
 Autor: Mariana Canchola Ramírez
 Fecha: 04 octubre 2022
 '''
 
+#Esto lo pegamos del laboratorio anterior.
 def is_year_leap(year):
     if year < 1582:
         return False
@@ -15,14 +18,14 @@ def is_year_leap(year):
         return False
     else: 
         return True
-
+#Estos son los ultimos días de los meses. los cuales están declarados en un array.
 def days_in_month(year, month):
     monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if is_year_leap(year) and month == 2:
             return 29
     else:
         return monthDays[month - 1]
-
+#la variable nos da años y días al azar para probar nuestro programa.
 test_years = [1900, 2000, 2016, 1987]
 test_months = [2, 2, 1, 11]
 test_results = [28, 29, 31, 30]
@@ -35,3 +38,4 @@ for i in range(len(test_years)):
 		print("OK")
 	else:
 		print("Fallido")
+#Declarado cada uno en su variable correspondiente obtendremos los resultados deseados.
