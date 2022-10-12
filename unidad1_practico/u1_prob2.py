9bfefc725cb4f722c9a73bd4c91e3d1bdc79d36c
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-10-12 15:23:32 -0500

    Update u1_prob2.py

--- unidad1_practico/u1_prob2.py
+++ unidad1_practico/u1_prob2.py
@@ -1,12 +1,14 @@
 '''
-Descripción: Lea un número y despliegue una secuencia de números de N hasta 1.
+Descripción: Numeros divisibles
+Numeros divisibles
+Realiza un programa en Python que indique si un número es divisible entre otro.
 Autor: Mariana Canchola Ramírez
 Fecha: 06 octubre 2022
 '''
 
 #Definimos las variables, con un input para que el usuario ingrese dos números al azar que se almacenaran en x y y
-x = int(input())
-y = int(input()) 
+x = int(input("Ingresa el primer número: "))
+y = int(input("Ingresa el segundo número: ")) 
 
 #Colocamos una condición, que nos ayude a determinar si x y y son diblisibles, el cual si es así mostrará un mensaje de que si son divisibles.
 def divisible(x, y):
