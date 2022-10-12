af955bbeffa803160785e4e234e2e235dbbb75ec
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-10-12 17:45:36 -0500

    Update lab4.3.1.8.py

--- modulo04/lab4.3.1.8.py
+++ modulo04/lab4.3.1.8.py
@@ -1,5 +1,7 @@
 '''
 Descripción: Día del año: escribiendo y utilizando tus propias funciones
+ escribir y probar una función que toma tres argumentos (un año, un mes y un día del mes) 
+ y devuelve el día correspondiente del año, o devuelve None si cualquiera de los argumentos no es válido.
 Autor: Mariana Canchola Ramírez
 Fecha: 04 octubre 2022
 '''
@@ -22,7 +24,7 @@ def days_in_month(year, month):
     if is_year_leap(year) and month == 2:
             return 29
     return monthDays[month - 1]
-
+#En esta parte del código asignamos una variable para sumarizarla.
 def day_of_year(year, month, day):
     if year < 1582:
         return None
@@ -31,12 +33,12 @@ def day_of_year(year, month, day):
     if day > days_in_month(year, month) or day < 1:
         return None
         
-    totalDays = day
+    totalDays = day #iguala el total de días para asignarla a una nueva variable
     month = month - 1
     while month > 0:
-        totalDays += days_in_month(year, month)
+        totalDays += days_in_month(year, month) #al total de días le sumará el año y meses, y lo devolverá en cifras.
         month -= 1
         
-    return totalDays
+    return totalDays #Devuelve sumando el total de días.
 
-print(day_of_year(2001, 2, 28))
+print(day_of_year(2001, 2, 28)) #aqui podemos cambiar este parametro para ver que fecha queremos verificar.
