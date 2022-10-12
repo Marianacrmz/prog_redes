545ee822e22823a92fd973dd2b0a3698c3cf6682
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-10-12 16:10:30 -0500

    Update u1_prob3.py

--- unidad1_practico/u1_prob3.py
+++ unidad1_practico/u1_prob3.py
@@ -4,6 +4,8 @@ Autor: Mariana Canchola Ramírez
 Fecha: 06 octubre 2022
 '''
 
+#Creamos una variable, insertamos su tipo y un input para que el usuario introduzca un número al azar
 num = int(input("Introduce un número entero que sea positivo: "))
+#Para esto hice un ciclo for,en el cual los datos se almacenaran en i, con la condicón de comenzar desde 0 y terminar en n número.
 for i in range(num, 0, -1):
     print(i, end=",")
