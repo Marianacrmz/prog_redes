55407a9bfb07554febcf79ea3c31b037930e8f6e
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-10-18 09:36:46 -0500

    Unidad II

---
+++ Unidad II/06_while-loop.py
@@ -0,0 +1,12 @@
+'''
+Descripción: Create a program with a while loop that counts to a user’s supplied number.
+Autor: Mariana Canchola Ramírez
+Fecha: 18 octubre 2022
+'''
+
+x=input("Enter a number to count to: ")
+x=int(x)
+y=1
+while y<=x:
+    print(y)
+    y=y+1
