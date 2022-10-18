78bc6e43dd75172723425517d27981fdd9503123
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-10-18 09:13:41 -0500

    Unidad II

--- Unidad II/03_personal-info.py
+++ Unidad II/03_personal-info.py
@@ -1,4 +1,4 @@
-'''
+''
 Descripción:Create a script that asks for four pieces of information such as: first name, last name, location, and age.
 Create a variable for a space: space = " "
 Add a print statement that that combines all the information in one sentence.
@@ -19,4 +19,4 @@ space = " "
 
 
 #Add a print statement that that combines all the information in one sentence.
-print("Hola " + name + space + lastName + " tu vives en " + loc + " y tienes " + edad + " años")
+print("Hola " + name + space + lastName + " tu vives en " + space + loc + " y tienes " + space + edad + " años")
