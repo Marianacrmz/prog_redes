3051b674dc97e02c06917aea7773eb5743eee207
Marianacrmz <106276727+Marianacrmz@users.noreply.github.com>
2022-11-12 00:18:12 -0600

    UNIDAD II

--- Unidad II/REST API/practica01_unidad2.py
+++ Unidad II/REST API/practica01_unidad2.py
@@ -3,19 +3,19 @@ Descripción de la API: API utilizada para personajes del anime.
 Nombre: Mariana Canchola Ramírez 
 Fecha de creación: 08 noviembre 2022
 '''
-
+#Añadí los módulos para agregarle la funcionalidad
 import requests
 personajes = "Gitama"
-json_status = 0
+json_status = 0 #variables
 
-
+#FORMACIÓN DE MI API
 url = "https://api.jikan.moe/v4/top/anime"
 data = requests.get(url)
 if data.status_code == 200:
     data = data.json()
     for e in data["data"]:
         print (e["title"])
-
+#Ciclo que le ayuda al usuario a salir cuando lo decida con la palabra "Salir" o "s"
 while True:
     personajes = input("Personaje correcto: ")
     print (e["title"])
